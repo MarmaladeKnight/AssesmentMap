43a6fb699a1e8e9a935a4a12f7a1cdfd2d59df8e
MarmaladeKnight <misha06052000@gmail.com>
2018-07-05 10:57:49 +0300

    cOMMIt for ejecting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+      let text = "dfsds";
+    return (
+      <div className="block">
+          {text}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import * from 'map.js'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
